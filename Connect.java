cc4d027ba466aa0d6026a13fc10da0d616f62517
NIBEDITA-JENA <deepi.nibedita074@gmail.com>
2017-06-22 15:29:50 +0530

    Add files via upload

---
+++ Connect.java
@@ -0,0 +1,10 @@
+
+
+public class Connect {
+    
+    public String getpath()
+    {
+       String path="E:/test/quizdb.accdb"; //specify where your database resides.
+       return path;
+    }
+}
